42df8736e6767de5b1c8fa10112ed50043369b5b
av.dias <av.dias@campus.fct.unl.pt>
2022-08-27 19:59:09 +0100

    updating sidebar to change path and style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
 import navLogo from "../images/logo.png";
+import { AiOutlineLogout } from "react-icons/ai";
 
 export default function Sidebar(props) {
   return (
     <div className="sidebar">
       <img src={navLogo} alt="logo" className="sidebar--logo" />
-      <p className="sidebar--button hover active">Dashboard</p>
-      <p className="sidebar--button hover">Moviments</p>
-      <p className="sidebar--button hover">Splits</p>
-      <p className="end sidebar--button hover">Login</p>
+      <a href="/Dashboard" className="sidebar--button hover active">
+        Dashboard
+      </a>
+      <a href="/Moviments" className="sidebar--button hover">
+        Moviments
+      </a>
+      <a href="/Splits" className="sidebar--button hover">
+        Splits
+      </a>
+      <div className="end align-list">
+        <a href="/Login" className="sidebar--button hover">
+          Login
+        </a>
+        <AiOutlineLogout size={20} color="gray" className="sidebar-icon" />
+      </div>
     </div>
   );
 }
