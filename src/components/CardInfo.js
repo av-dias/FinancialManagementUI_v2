07e8a3060dfa4473ed438c1ba45886dd0eb05584
av.dias <av.dias@campus.fct.unl.pt>
2022-08-29 18:43:18 +0100

    cardinfo component

---
+++ src/components/CardInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./CardInfo.css";
+
+export default function Card(props) {
+  return (
+    <div className="cardinfo">
+      <div>
+        {props.text && <span>{props.text}</span>}
+        {props.description && (
+          <div className="center bold">
+            <br></br>
+            <span>{props.description}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
