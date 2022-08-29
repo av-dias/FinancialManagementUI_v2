36f41bb66fbcc6d11e65353e98ad3503a615cfd7
av.dias <av.dias@campus.fct.unl.pt>
2022-08-29 18:44:55 +0100

    pop up refactoring with grid

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./Popup.css";
 import { IoIosCloseCircle } from "react-icons/io";
+import Grid from "@mui/material/Grid";
 
 const Popup = (props) => {
   return (
     <>
       <div className="popup-box">
         <div className="box">
-          <span className="close-icon" onClick={props.handleClose}>
-            <IoIosCloseCircle size={30} />
-          </span>
-          {props.content}
+          <Grid container spacing={{ xs: 2, sm: 2, md: 2 }} columns={24}>
+            <Grid item xs sm md></Grid>
+            <Grid item xs={6} sm={6} md={6}>
+              {props.content}
+            </Grid>
+            <Grid item xs={1} sm={1} md={1}>
+              <span className="close-icon" onClick={props.handleClose}>
+                <IoIosCloseCircle size={30} />
+              </span>
+            </Grid>
+            <Grid item xs sm md></Grid>
+          </Grid>
         </div>
       </div>
     </>
