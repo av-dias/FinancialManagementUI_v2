50b39ec75c3581850a72d4a7ea277f8c78909e50
av.dias <av.dias@campus.fct.unl.pt>
2022-09-24 20:21:27 +0100

    minor

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,7 +6,6 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import ButtonOutline from "../components/Button";
 
 import { truncateMax } from "../api/moviment.api";
 import STATUS from "../utility/status";
