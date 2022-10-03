7e99f9531f8af741b96119c5059dd46bea7a7515
av.dias <av.dias@campus.fct.unl.pt>
2022-10-03 17:55:47 +0100

    route protection

--- src/App.js
+++ src/App.js
@@ -6,15 +6,37 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Moviments from "./pages/Moviments";
 import Splits from "./pages/Splits";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/Moviments" element={<Moviments />} />
-        <Route path="/Splits" element={<Splits />} />
+        <Route
+          path="/Dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/Moviments"
+          element={
+            <ProtectedRoute>
+              <Moviments />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/Splits"
+          element={
+            <ProtectedRoute>
+              <Splits />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute(children) {
+  if (
+    window.sessionStorage.getItem("user_id") &&
+    window.sessionStorage.getItem("user_name") &&
+    window.sessionStorage.length === 4
+  ) {
+    return children;
+  } else {
+    return <Navigate to="/" replace />;
+  }
+}
