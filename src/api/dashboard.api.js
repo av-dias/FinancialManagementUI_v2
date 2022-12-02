adce5a7ce88ac0e2034ceeff861ff9e4864c947b
av.dias <av.dias@campus.fct.unl.pt>
2022-12-02 17:41:51 +0000

    [adaptation] Adjust dashboard to transaction feature

--- src/api/dashboard.api.js
+++ src/api/dashboard.api.js
@@ -51,6 +51,28 @@ export async function loadData(month) {
     const data = await response.json();
     data["total"] = dataSplit;
     data["transactions"] = dataTransactions;
+
+    // Rechecks all savings and spendings and adjusts with the transactions
+    for (let i = 1; i <= 12; i++) {
+      if (isNaN(data["transactions"].per_month[i])) continue;
+
+      data["purchases_by_month"][i] -= data["transactions"].per_month[i];
+      data["savings_by_month"][i] += data["transactions"].per_month[i];
+    }
+
+    // Rechecks all total savings and spendings
+    if (data["transactions"].total > 0) {
+      data["total_savings"] += data["transactions"].total;
+      data["total_spendings"] += data["transactions"].total;
+    } else {
+      data["total_savings"] -= data["transactions"].total;
+      data["total_spendings"] -= data["transactions"].total;
+    }
+
+    // Updates months's savings and spendings
+    data["month_savings"] = data["savings_by_month"][month];
+    data["month_spendings"] = data["purchases_by_month"][month];
+
     console.log(data);
     return data;
   } catch (e) {
