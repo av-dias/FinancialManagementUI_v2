f62af9d61a71c03cee849dad13285b5b888d64e0
av.dias <av.dias@campus.fct.unl.pt>
2023-01-10 21:16:53 +0000

    [feature] adding new stats calcs

--- src/api/dashboard.api.js
+++ src/api/dashboard.api.js
@@ -4,47 +4,48 @@ export async function loadData(month) {
   try {
     let user_id = window.sessionStorage.getItem("user_id");
 
-    let response = await fetch(
-      `http://${ADDRESS.BACKEND}/api/v1/user/${user_id}/purchase/statistics/month/${month}`,
-      {
-        method: "GET",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer " + window.sessionStorage.getItem("access_token"),
-        },
-      }
-    );
+    let response = await fetch(`http://${ADDRESS.BACKEND}/api/v1/user/${user_id}/purchase/statistics/month/${month}`, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + window.sessionStorage.getItem("access_token"),
+      },
+    });
 
-    let resSplit = await fetch(
-      `http://${ADDRESS.BACKEND}/api/v1/split/purchases/user/${user_id}/month/${month}`,
-      {
-        method: "GET",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer " + window.sessionStorage.getItem("access_token"),
-        },
-      }
-    );
+    let resSplit = await fetch(`http://${ADDRESS.BACKEND}/api/v1/split/purchases/user/${user_id}/month/${month}`, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + window.sessionStorage.getItem("access_token"),
+      },
+    });
 
-    let resTransactions = await fetch(
-      `http://${ADDRESS.BACKEND}/api/v1/transactions/user/${user_id}`,
-      {
-        method: "GET",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer " + window.sessionStorage.getItem("access_token"),
-        },
-      }
-    );
+    let resTransactions = await fetch(`http://${ADDRESS.BACKEND}/api/v1/transactions/user/${user_id}`, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + window.sessionStorage.getItem("access_token"),
+      },
+    });
+
+    let r = await fetch(`http://${ADDRESS.BACKEND}/api/v1/purchase/user/${user_id}/stats`, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + window.sessionStorage.getItem("access_token"),
+      },
+    });
+    const rTest = await r.json();
+
+    console.log(rTest);
 
     const dataSplit = await resSplit.json();
     const dataTransactions = await resTransactions.json();
@@ -73,7 +74,7 @@ export async function loadData(month) {
     data["month_savings"] = data["savings_by_month"][month];
     data["month_spendings"] = data["purchases_by_month"][month];
 
-    console.log(data);
+    //console.log(data);
     return data;
   } catch (e) {
     console.log(e.message);
