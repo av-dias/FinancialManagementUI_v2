8d6c8d596f9a4471b71638c832d53872592d2018
av.dias <av.dias@campus.fct.unl.pt>
2023-06-19 19:07:53 +0100

    [Network] Changes so its easier to have a central server

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 
 import { clearElementValueById } from "../functions/elements";
 
+import ADDRESS from "../utility/address";
+
 export default function Splits() {
   const navigate = useNavigate();
   window.sessionStorage.clear();
@@ -32,34 +34,29 @@ export default function Splits() {
             <label className="padding" htmlFor="pname">
               Email
             </label>
-            <input
-              className="padding"
-              type="text"
-              id="email"
-              name="email"
-            ></input>
+            <input className="padding" type="text" id="email" name="email"></input>
             <label className="padding" htmlFor="tname">
               Password
             </label>
-            <input
-              className="padding"
-              type="password"
-              id="password"
-              name="password"
-            ></input>
+            <input className="padding" type="password" id="password" name="password"></input>
             <div className="center">
-              <button
-                className="padding"
-                type="submit"
-                name="save"
-                value="Saveeeee"
-              >
+              <button className="padding" type="submit" name="save" value="Saveeeee">
                 Login
               </button>
             </div>
           </form>
         </Grid>
         <Grid item xs={5} sm={5} md={5}></Grid>
+        <Grid item xs={15} sm={15} md={15}>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="center bold-ip">
+            <h1> {ADDRESS.IP} </h1>
+          </div>
+        </Grid>
       </Grid>
     </div>
   );
--- src/utility/address.js
+++ src/utility/address.js
@@ -1,4 +1,6 @@
 module.exports = {
-  BACKEND: "localhost:8080",
+  BACKEND: window.location.hostname + ":8080",
+  BACKEND0: "localhost:8080",
   BACKEND1: "192.168.51.232:8080",
+  IP: window.location.hostname,
 };
